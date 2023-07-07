a4b930a232458387e4e602deb33c939aca76146e
pavan <pavanareti02@gmail.com>
2023-07-07 16:47:14 +0530

    going to convert toggle from checkbox to normal

--- js/index.js
+++ js/index.js
@@ -1,18 +1,23 @@
 function toggleContent() {
   let checkboxes = document.querySelectorAll('input[type="checkbox"]');
   let checked = document.querySelectorAll('input[type="checkbox"]:checked');
-  
-    if (checked.length === 0) {
-        checkboxes.forEach(checkbox => {
-            checkbox.checked = true;
-        }
-        )
-    } else {
-        checkboxes.forEach(checkbox => {
-            checkbox.checked = false;
-        }
-        )
-    }
-  
+  let btns = document.querySelectorAll(".toggle-content-btn__text");
 
+  if (checked.length === 0) {
+    btns.forEach((btn) => {
+      btn.innerHTML =
+        'See less features <span><i class="fa fa-angle-up" aria-hidden="true"></i></span>';
+    });
+    checkboxes.forEach((checkbox) => {
+      checkbox.checked = true;
+    });
+  } else {
+    btns.forEach((btn) => {
+      btn.innerHTML =
+        'See all features <span><i class="fa fa-angle-down" aria-hidden="true"></i></span>';
+    });
+    checkboxes.forEach((checkbox) => {
+      checkbox.checked = false;
+    });
+  }
 }
